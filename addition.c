91c33e94c8a0d8e1a7f450e651bea85beaf99117
Jennisxavier <jennisxavier2512@gmail.com>
2023-07-18 15:04:16 +0530

    18.7

---
+++ addition.c
@@ -0,0 +1,13 @@
+/*addition of two integers*/
+#include <stdio.h>
+int main()
+{
+    int f,s,sum;
+    printf("enter the first no: ");
+    scanf("%d",&f);
+    printf("enter the second no:");
+    scanf("%d",&s);
+    sum=f + s;
+    printf("the sum is:%d",sum);
+    return 0;
+}

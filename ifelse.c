dff994e65a7f0251d6974b2b132748cebf10dd39
Jennisxavier <jennisxavier2512@gmail.com>
2023-07-19 21:06:20 +0530

    19.7

---
+++ ifelse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>    
+int main(){
+int a = 8;
+if (a == 10 ) {
+    printf("hello");
+}
+if ( a < 10) {
+    printf("hello less than");
+}
+if (a == 8) {
+    printf("equal to 8");
+}
+
+    return 0;
+}
